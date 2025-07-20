ac77195469046bb158c1c587fd42f73480cfaff1
avi-v2 <avi4anand@gmail.com>
2025-07-20 11:47:04 +0530

    Added logic

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import random
 from words import words
 import string
-def valid(w):
+def valid():
     word=random.choice(words)
     while '-' in word or ' ' in word:
-        word=random.choice(words)
+        word=(random.choice(words))
+    return list(word.upper())
 
-    return word
-
-valid_guess=list(string.ascii_uppercase)
+valid_guess=(string.ascii_uppercase)
 # print(valid_guess)
-user=input("Enter your guess:\t").upper()
-if user not in valid_guess:
-     print("Invalid input: ")
+word=(valid())
+
+ans=[]
+for i in word:
+    ans.append("_")
+print(word)
+guessed=[]
+while True:
+    print(f"The word is {ans}")
+    user=input("Enter your guess:\t").upper()
+    if len(guessed)>0:
+        print("guessed words:", guessed)
+    if user not in valid_guess:
+        print("Invalid input: ")
+    if user not in guessed:
+        guessed.append(user)
+    else:
+        print("Letter already guessed")
+    for i,l in enumerate(word):
+        if l==user:
+            ans[i]=l
+    if "_" not in ans:
+        print("You Won")
+        print("The word was ", word)
